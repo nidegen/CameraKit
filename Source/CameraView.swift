b457c55b501dcfa84159a4f8db8c4f8cc66e62a5
Nicolas Degen <ndegen@me.com>
2020-02-03 23:35:01 +0100

    added siwftui view

---
+++ Source/CameraView.swift
@@ -0,0 +1,25 @@
+//
+//  CameraView.swift
+//  CameraKit
+//
+//  Created by Nicolas Degen on 03.02.20.
+//  Copyright © 2020 Nicolas Degen. All rights reserved.
+//
+
+import SwiftUI
+
+public struct CameraView: UIViewRepresentable {
+  var cameraManager: CameraManager
+  
+  public init(cameraManager: CameraManager) {
+    self.cameraManager = cameraManager
+  }
+  
+  public func makeUIView(context: Context) -> UIView {
+    let view = UIView()
+    cameraManager.addPreviewLayer(view: view)
+    return view
+  }
+  
+  public func updateUIView(_ uiView: UIView, context: Context) {}
+}
